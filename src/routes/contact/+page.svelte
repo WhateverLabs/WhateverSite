aecd4827155c0cc18c4d5de6aa1b6e27c2e21788
httpjamesm <github@httpjames.space>
2023-05-21 22:53:50 -0400

    feat: contact

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,56 @@
+<div class="content">
+	<div class="child">
+		<p>
+			All e-mails will be received in the same inbox, but choosing the right address will help us
+			distinguish the nature of the request more easily.
+		</p>
+		<p>
+			Feel free to contact the admin for general inquiries and issues at <a
+				href="mailto:hello@whatever.social">hello@whatever.social</a
+			>
+		</p>
+		<p>
+			If you are emailing in regards to an inquiry as a law enforcement agency, please e-mail
+			<a href="mailto:legal@whatever.social">legal@whatever.social</a>.
+		</p>
+		<h3>PGP Public Key - Send us your e-mail securely</h3>
+		<code
+			>-----BEGIN PGP PUBLIC KEY BLOCK-----
+			xjMEY2YcxRYJKwYBBAHaRw8BAQdAcsXp6irjbjiAoRQs3/4loB55Lat9wmN4
+			yRKQ1i25WmnNNWl0c3doYXRldmVydGltZUBwcm90b24ubWUgPGl0c3doYXRl
+			dmVydGltZUBwcm90b24ubWU+wowEEBYKAD4FAmNmHMUECwkHCAkQwNeKfKFb
+			rTUDFQgKBBYAAgECGQECGwMCHgEWIQQxwUyJpmnbzfPK78rA14p8oVutNQAA
+			rogBAJ8f4bEGm/xK6mpF5oow9+4SEbcjVewcZwwOvykEMO/cAP9o1wGGLlsW
+			C1toJZAFZC9lqWu5DkZIn7ZWggAZCtuyBs44BGNmHMUSCisGAQQBl1UBBQEB
+			B0AmYeB6l3gaPidCUE+2R1BVqdKH7ztYcVarjTDBabVuDQMBCAfCeAQYFggA
+			KgUCY2YcxQkQwNeKfKFbrTUCGwwWIQQxwUyJpmnbzfPK78rA14p8oVutNQAA
+			2IABAOeWU4NTTWUTTCtlakWf8yfP9BzShawSc9LcCu7flfp5AQCzPQ74cpYm
+			eijbRccmQfUSiWCtqMaIdsm8Rtx3qvBUAA== =9dkg -----END PGP PUBLIC KEY BLOCK-----
+		</code>
+	</div>
+</div>
+
+<style lang="scss">
+	@import '../../vars.scss';
+
+	.content {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		box-sizing: border-box;
+		padding: 5rem;
+
+		@media only screen and (max-width: $mobile-width) {
+			padding: 1rem;
+		}
+
+		code {
+			word-break: break-all;
+			background-color: #353535;
+			box-sizing: border-box;
+			width: 100%;
+		}
+	}
+</style>
