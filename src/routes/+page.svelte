bdf9fd7764f8e6735531abdb43e5e3490fe50c38
httpjamesm <github@httpjames.space>
2023-05-21 22:19:12 -0400

    style: scaling issues on desktop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <body class="page1">
 	<header>
-		<a href="/"> <img src="images/Whatever-Logo.svg" alt="Whatever Social Logo" class="logo"/></a>
+		<a href="/"> <img src="images/Whatever-Logo.svg" alt="Whatever Social Logo" class="logo" /></a>
 		<nav class="navbar">
 			<ul class="nav__list">
 				<li class="nav__list-item">
@@ -54,7 +54,7 @@
 				</div>
 			</div>
 			<ul id="scene">
-				<img src="images/banner.png" alt="Banner"/>
+				<img src="images/banner.png" alt="Banner" />
 			</ul>
 		</div>
 	</section>
@@ -201,10 +201,10 @@
 		background-color: $background-color_3;
 	}
 	.hero-elements {
-		box-sizing: border-box;
 		height: 100vh;
 		justify-content: space-between;
 		display: flex;
+		width: 100%;
 	}
 	.tagline {
 		float: left;
@@ -217,19 +217,19 @@
 		width: 12em;
 		font-size: 3.5rem;
 		line-height: 1.2em;
-		z-index: 2;
+	}
+
+	.first__section--pg1 {
+		width: 100%;
 	}
 	.phone {
-		float: right;
-		display: flex;
-		justify-content: space-between;
-		padding-top: 1.5em;
-		margin-top: 1em;
 		width: 100%;
-		max-width: 900px;
-		min-width: 800px;
-		margin-right: 2em;
-		z-index: 2;
+
+		display: flex;
+		align-items: center;
+		img {
+			width: 100%;
+		}
 	}
 	.banner {
 		#scene {
@@ -333,33 +333,27 @@
 		vertical-align: middle;
 	}
 	.third__section--pg1 {
-		display: grid;
-		margin-left: 2em;
-		margin-right: 2em;
-		padding-bottom: 4em;
-		grid-template-columns: 1fr 1fr;
-		grid-template-rows: 1fr;
-		gap: 0px 40px;
-		grid-template-areas: "world-map textbox-4";
+		.container {
+			display: flex;
+			padding: 2rem;
+			box-sizing: border-box;
+            
+			.world-map {
+				grid-area: world-map;
+				height: auto;
+				width: auto;
+				max-width: 700px;
+			}
+			.textbox-4 {
+				color: $color_4;
+				text-align: right;
+				font-size: 2.5rem;
+				margin-bottom: 0;
+			}
+		}
+
 		background-color: $background-color_1;
 		font-family: $font-family_1;
-
-		.world-map {
-			grid-area: world-map;
-			height: auto;
-			width: auto;
-			max-width: 700px;
-		}
-		.textbox-4 {
-			grid-area: textbox-4;
-			line-height: 1.2em;
-			text-align: right;
-			margin-left: unset;
-			margin-right: unset;
-			color: $color_4;
-			font-size: 2.5rem;
-			margin-bottom: 0;
-		}
 	}
 	footer {
 		background-color: $background-color_2;
@@ -398,196 +392,6 @@
 		&:hover {
 			transition: 0.2s ease;
 			color: $color_3;
-		}
-	}
-	@media (max-width: 1100px) {
-		.burger {
-			display: block;
-		}
-		.bar {
-			transition: all 0.2s ease-in-out;
-		}
-		.burger.active {
-			.bar {
-				&:nth-child(2) {
-					opacity: 0;
-				}
-				&:nth-child(1) {
-					transform: translateY(13px) rotate(45deg);
-				}
-				&:nth-child(3) {
-					transform: translateY(-13px) rotate(-45deg);
-				}
-			}
-		}
-		.nav__list {
-			opacity: 0;
-			position: fixed;
-			left: -200vh;
-			top: 157.94px;
-			gap: 0;
-			min-width: 100%;
-			height: 50vh;
-			flex-direction: column;
-			color: $color_4;
-			background-color: $background-color_1;
-			padding-top: 1em;
-			padding-bottom: 1em;
-			text-align: center;
-			align-items: center;
-		}
-		.nav__link {
-			margin: 0;
-		}
-		.nav__list.active {
-			left: 0;
-			margin: 0;
-			opacity: 100%;
-		}
-	}
-	@media (max-width: 1350px) {
-		.hero-elements {
-			font-size: 2rem;
-			color: $color_4;
-			justify-content: unset;
-			display: inline-block;
-			padding: 2em 0.8em;
-			padding-top: 5em;
-			padding-bottom: 0;
-			box-sizing: border-box;
-			height: 100%;
-			text-align: center;
-			align-items: center;
-		}
-		.banner {
-			#scene {
-				opacity: 0;
-			}
-		}
-		.waves {
-			margin-top: 0;
-		}
-		.phone {
-			float: unset;
-			display: block;
-			text-align: center;
-			justify-content: space-between;
-			padding-top: 3em;
-			width: unset;
-			height: unset;
-			max-width: unset;
-			min-width: unset;
-			max-height: unset;
-			padding-left: unset;
-		}
-		.tagline {
-			display: inline;
-			max-inline-size: unset;
-			padding-top: unset;
-			color: $color_4;
-			letter-spacing: unset;
-			margin-top: unset;
-			margin-left: unset;
-			width: unset;
-			font-size: 2.5rem;
-			float: unset;
-			line-height: 1.2em;
-		}
-		.second__section--text {
-			display: grid;
-			justify-content: center;
-			align-items: center;
-			margin-left: 2em;
-			margin-right: 2em;
-			grid-auto-rows: 1fr;
-			grid-template-columns: 1fr;
-			grid-template-rows: 1fr;
-			gap: 20px 40x;
-			grid-template-areas: 'textbox-1' 'textbox-2';
-		}
-		.textbox-1 {
-			grid-area: textbox-1;
-			text-align: center;
-			box-shadow: 0px 10px white;
-		}
-		.textbox-2 {
-			grid-area: textbox-2;
-			text-align: center;
-			box-shadow: 0px 10px white;
-		}
-		.cell-2 {
-			grid-area: cell-2;
-			display: none;
-		}
-		.cell-3 {
-			grid-area: cell-3;
-			display: none;
-		}
-		.textbox-3 {
-			text-align: center;
-			max-inline-size: unset;
-			color: $color_4;
-			letter-spacing: unset;
-			font-size: 2rem;
-			margin-left: 1.5em;
-			margin-right: 1.5em;
-			margin-top: 10em;
-			margin-bottom: unset;
-			float: unset;
-			width: unset;
-			line-height: 1em;
-		}
-		.third__section--pg1 {
-			display: grid;
-			margin-left: 2em;
-			margin-right: 2em;
-			padding-bottom: 4em;
-			align-items: center;
-			justify-items: center;
-			grid-template-columns: 1fr;
-			gap: 0px 40px;
-			grid-template-areas: 'world-map' 'textbox-4';
-		}
-		.world-map {
-			display: block;
-			text-align: center;
-			justify-content: center;
-			width: 100%;
-			height: auto;
-		}
-		.textbox-4 {
-			text-align: center;
-			margin-left: 1em;
-			margin-right: 1em;
-			color: $color_4;
-			font-size: 1.5rem;
-			margin-bottom: 0;
-		}
-		.footer__list-logo {
-			display: none;
-		}
-		.footer__list {
-			gap: 40px;
-			font-size: 1rem;
-			letter-spacing: 1.5px;
-		}
-	}
-	@media (max-width: 400px) {
-		.phone {
-			max-width: 300px;
-		}
-	}
-	@media (min-width: 1900px) {
-		.phone {
-			margin-right: 4em;
-		}
-		.tagline {
-			margin-left: 4em;
-		}
-	}
-	@media (max-width: 600px) {
-		#lock {
-			max-width: 70%;
 		}
 	}
 </style>
