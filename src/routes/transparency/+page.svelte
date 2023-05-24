33f085944765e524d3ab40b0d7cd66c01f6f5dd0
httpjamesm <github@httpjames.space>
2023-05-24 16:26:31 -0400

    style: transparency layout

--- src/routes/transparency/+page.svelte
+++ src/routes/transparency/+page.svelte
@@ -1,36 +1,42 @@
-<div class="container">
-	<a href="/"> <img src="images/favicon.png" alt="Whatever Social Logo" class="logo" /></a>
-	<h2 class="title">Transparency Report</h2>
-	<br />
-	<h3>2022</h3>
-	<p>
-		In 2022, we received 0 requests from government and/or law enforcement agencies.
-	</p>
-    <br />
-    <h3>2023</h3>
-    <p>
-		As of March 30th, 2023, we have received 0 requests from government and/or law enforcement agencies this year.
-	</p>
+<div class="parent">
+	<div class="container">
+		<a href="/"> <img src="images/favicon.png" alt="Whatever Social Logo" class="logo" /></a>
+		<h2 class="title">Transparency Report</h2>
+		<br />
+		<h3>2022</h3>
+		<p>In 2022, we received 0 requests from government and/or law enforcement agencies.</p>
+		<br />
+		<h3>2023</h3>
+		<p>
+			As of March 30th, 2023, we have received 0 requests from government and/or law enforcement
+			agencies this year.
+		</p>
+	</div>
 </div>
 
 <style lang="scss">
-	.container {
+	.parent {
 		display: flex;
 		justify-content: center;
-        width: 30rem;
+		width: 100%;
 		padding-top: 1rem;
+		box-sizing: border-box;
+
+		.container {
+			max-width: 30rem;
+			.title {
+				text-align: center;
+			}
+			.logo {
+				width: 2em;
+				margin-left: auto;
+				margin-right: auto;
+				display: block;
+			}
+		}
+
 		@media only screen and (max-width: 800px) {
 			padding: 1rem;
-			box-sizing: border-box;
 		}
-        .title {
-            text-align: center;
-        }
-        .logo {
-            width: 2em;
-            margin-left: auto;
-		    margin-right: auto;
-            display: block;
-        }
 	}
 </style>
