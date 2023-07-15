63006420b8885522290ec05ed6bc4e99f57f11b1
Whatever Social <109485589+privacytime101@users.noreply.github.com>
2023-07-14 23:25:41 -0700

    Update donate page

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,59 +1,13 @@
-<script lang="ts">
-	const copyMonero = () => {
-		try {
-			navigator.permissions
-				.query({
-					// @ts-ignore
-					name: 'clipboard-write'
-				})
-				.then((r) => {
-					if (r.state == 'granted' || r.state == 'prompt')
-						navigator.clipboard
-							.writeText(
-								'87tu5QTMSKmi8RmyyRCP1daBouRKegmL54k9d4cqqWscHWTqSaKRXQu9X2g81JgJyfQFbyL1dTRmR6TsZ8oZZqLj1Ljfyd7'
-							)
-							.then(function (m) {
-								alert('The Monero address was beamed to your device. Paste away!');
-							});
-					else
-						try {
-							navigator.clipboard
-								.writeText(
-									'87tu5QTMSKmi8RmyyRCP1daBouRKegmL54k9d4cqqWscHWTqSaKRXQu9X2g81JgJyfQFbyL1dTRmR6TsZ8oZZqLj1Ljfyd7'
-								)
-								.then(function (m) {
-									alert(
-										"Whoops, we're not sure if this copied in your browser. Here's the address just in case: 87tu5QTMSKmi8RmyyRCP1daBouRKegmL54k9d4cqqWscHWTqSaKRXQu9X2g81JgJyfQFbyL1dTRmR6TsZ8oZZqLj1Ljfyd7"
-									);
-								});
-						} catch {
-							alert(
-								"For some reason we couldn't copy the Monero address to your clipboard. Here's the ID: 87tu5QTMSKmi8RmyyRCP1daBouRKegmL54k9d4cqqWscHWTqSaKRXQu9X2g81JgJyfQFbyL1dTRmR6TsZ8oZZqLj1Ljfyd7"
-							);
-						}
-				});
-		} catch {
-			navigator.clipboard
-				.writeText(
-					'87tu5QTMSKmi8RmyyRCP1daBouRKegmL54k9d4cqqWscHWTqSaKRXQu9X2g81JgJyfQFbyL1dTRmR6TsZ8oZZqLj1Ljfyd7'
-				)
-				.then(function (m) {
-					alert('The Monero address was beamed to your device. Paste away!');
-				});
-		}
-	};
-</script>
-
 <div class="content">
 	<div class="child">
 		<p>
-			As a student, mounting server costs may become a problem in the future. If you'd like to
+			As students, mounting server costs may become a problem in the future. If you'd like to
 			support this project's growth, please consider donating via <a
 				href="https://liberapay.com/whateversocial"
 				target="_blank"
 				rel="noopener noreferrer">Liberapay</a
 			>
-			or <a href="#" on:click={copyMonero}>Monero</a>.
+			or Monero (<code>Monero</code>).
 		</p>
 	</div>
 </div>
